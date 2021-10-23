cba08f212d004950d795e52aab43a73e8cbb8433
Wes Warriner <fridgehistory@gmail.com>
2021-10-23 16:44:34 -0700

    add dotenv implementation and local settings

---
+++ settings.py
@@ -0,0 +1,12 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+DB_USER = os.environ.get("DB_USER")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
+DB_NAME = os.environ.get("DB_NAME")
